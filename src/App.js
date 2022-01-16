8f5910f1dc9849c32070be9dfa27a95b1ad4ad74
Wiebke Bienk <wiebke.bienk@gmail.com>
2022-01-16 19:40:03 +0100

    Aufgabe b erledigt

--- src/App.js
+++ src/App.js
@@ -1,7 +1,52 @@
 import logo from './logo.svg';
 import './App.css';
+const {isbn10Checksum, isbn13Checksum} = require('isbn-check/src/isbn-check')
 
 function App() {
+//----Aufgabe b----//
+  //Textfeld/input
+  <textarea name="Isbn-Checker" id="checksum"></textarea>
+  //Button
+  Button.onclick = function(){ 
+  const isbn10Checksum = () => {
+    if (checksum.length < 10) {
+      ISBN13.isbn13Checksum();
+    }
+      if(checksum == "X") {
+        setIsValid(true);
+        document.getElementById("checksum").style.background='#00f700'
+      }
+      else if (checksum % 11 == 0) {
+        setIsValid(true);
+        document.getElementById("checksum").style.background='#00f700'
+      } 
+    else (checksum % 11 != 0); {
+      setIsValid(false);
+      document.getElementById("checksum").style.background='#ff0000'
+    };
+  }
+  }
+  };
+
+
+const isbn13Checksum = () => {
+  if (checksum.length < 13) {
+    ISBN10.isbn10Checksum();
+  }
+    if(checksum == "X") {
+      setIsValid(true);
+      document.getElementById("checksum").style.background='#00f700'
+    }
+    else if (checksum % 14 == 0) {
+      setIsValid(true);
+      document.getElementById("checksum").style.background='#00f700'
+    } 
+  else (checksum % 14 != 0); {
+    setIsValid(false);
+    document.getElementById("checksum").style.background='#ff0000'
+  };
+};
+  
   return (
     <div className="App">
       <header className="App-header">
@@ -20,6 +65,5 @@ function App() {
       </header>
     </div>
   );
-}
 
 export default App;
